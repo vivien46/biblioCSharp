4f9741b9ae559d29ae4eacb4b675a597309a9855
vivien <vlad-vivien@orange.fr>
2024-06-03 16:39:25 +0200

    add Book Component + BookController + move assets to public

--- Client/src/layout/layout.tsx
+++ Client/src/layout/layout.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import UserPage from '../Component/UserPage';
 import HomePage from '../Component/HomePage';
 import RegisterForm from '../Component/RegisterForm';
+import BookPage from '../Component/BookPage';
+import BookAdd from '../Component/BookAdd';
 
 const Layout: React.FC = () => {
     return (
@@ -13,6 +15,8 @@ const Layout: React.FC = () => {
                     <ul className="flex uppercase"> {/* Remove flex-row */}
                         <li><Link to="/" className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 transition duration-150">Home</Link></li>
                         <li><Link to="/api/user" className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 transition duration-150">Users</Link></li>
+                        <li><Link to="/api/book" className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 transition duration-150">Books</Link></li>
+                        <li><Link to="/api/book/add" className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 transition duration-150">Add books</Link></li>
                     </ul>
                 </nav>
             </header>
@@ -21,7 +25,9 @@ const Layout: React.FC = () => {
                 <Routes>
                     <Route path='/' element={<HomePage />}/>
                     <Route path='/api/user' element={<UserPage />}/>
-                    <Route path='/api/user/register' element={<RegisterForm />}/>
+                    <Route path='/api/user/register' element={<RegisterForm />} />
+                    <Route path='/api/book' element={<BookPage />} />
+                    <Route path='/api/book/add' element={<BookAdd />} />
                 </Routes>
             </main>
         </div>

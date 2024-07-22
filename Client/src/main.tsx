3f2018b6ba57bf32ff3a2c0e3f86cdffbeae4847
vivien46 <vlad-vivien@orange.fr>
2024-07-22 16:07:21 +0200

    fix auth reaction time + restruct main, app, layout files

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { AuthProvider } from './Contexts/AuthContext';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./Contexts/AuthContext";
+import "./index.css";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement!);
+
+root.render(
   <React.StrictMode>
     <AuthProvider>
-    <App />
+      <App />
     </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

b4747a037f26399961176897220d86b2cb944499
vivien <vlad-vivien@orange.fr>
2024-08-01 20:35:17 +0200

    add book poster in Emprunt List

--- Client/src/Pages/BooksPage.tsx
+++ Client/src/Pages/BooksPage.tsx
@@ -5,7 +5,6 @@ const BookPage: React.FunctionComponent = () => {
 
   return (
     <div className="flex flex-col justify-center">
-      <h1 className='text-3xl font-bold mb-4'>Books</h1>
       <BookList />
     </div>
   );
